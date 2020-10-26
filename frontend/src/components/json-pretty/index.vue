0047f5d503b8f66b48beedbf49987c33d1cf9015
kate <cat.katecc@gmail.com>
2020-10-26 16:30:38 -0400

    vue json pretty

---
+++ frontend/src/components/json-pretty/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <VueJsonPretty :data="data"/>
+</template>
+
+<script>
+import 'vue-json-pretty/lib/styles.css'
+
+export default {
+  name: "contract-logs",
+  props: {
+    data: Object,
+  },
+  components:  {
+    VueJsonPretty: () => import('vue-json-pretty')
+  },
+};
+</script>
