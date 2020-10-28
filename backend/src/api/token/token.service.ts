ecbbb271233819d87d31b250609a19bd4ca597e9
fsn365 <fsn365.com>
2020-10-28 04:46:11 -0400

    add endpoint for  getting token snapshot list

--- backend/src/api/token/token.service.ts
+++ backend/src/api/token/token.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { TokensHoldersQueryDto, QueryCmdDto } from '../../common';
-import { PgTokenService, PgTxsService } from '../../shared';
+import { QueryCmdDto } from '../../common';
+import { PgTokenService, PgTxsService, RedisHelperService } from '../../shared';
 import { ITxs } from '../../models';
 
 @Injectable()
@@ -9,6 +9,7 @@ export class TokenService {
     private pgToken: PgTokenService,
     private tx: PgTxsService,
     private token: PgTokenService,
+    private redis: RedisHelperService,
   ) {}
 
   getTokenByTokenHash(hash: string): Promise<any> {
@@ -17,6 +18,27 @@ export class TokenService {
 
   getTokens(query: QueryCmdDto): Promise<any[]> {
     return this.pgToken.getTokens(query);
+  }
+
+  getTokenSnapshots(
+    tokenIds: string[],
+  ): Promise<{ [key: string]: { symbol: string; precision: number } }> {
+    const tokens = tokenIds.filter(function(hash) {
+      return hash.length === 42 || hash.length === 66;
+    });
+    const promises = tokens.map(token => this.getTokenSnapshot(token));
+    const map = {};
+    return Promise.all(promises)
+      .then(snapshotList => {
+        snapshotList.map(item => {
+          if (item) {
+            const { token, ...snapshot } = item;
+            map[token] = snapshot;
+          }
+        });
+        return map;
+      })
+      .catch(e => ({}));
   }
 
   getTokenHolders(hash: string, query: QueryCmdDto) {
@@ -33,5 +55,32 @@ export class TokenService {
         total: stats.txs,
       };
     });
+  }
+
+  // dirty code, workers store the token snapshot in a shared redis instance
+  private getTokenSnapshot(
+    token: string,
+  ): Promise<{ symbol: string; precision: number; token: string }> {
+    const redisKey = `token:${token}:snapshot`;
+    return this.redis
+      .getCachedValue(`token:${token}:snapshot`)
+      .then(val => {
+        if (val) {
+          const snapshot = JSON.parse(val);
+          return { ...snapshot, token };
+        }
+        return this.pgToken
+          .getToken(token)
+          .then(data => {
+            const { symbol, precision } = data;
+            this.redis.cacheValue(
+              redisKey,
+              JSON.stringify({ symbol, precision }),
+            );
+            return { symbol, precision, token };
+          })
+          .catch(e => null);
+      })
+      .catch(e => ({}));
   }
 }
