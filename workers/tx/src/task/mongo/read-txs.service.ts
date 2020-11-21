5fc3918fd6d7374157c336622ec62dc9a395f0f8
fsn365 <fsn365.com>
2020-11-21 00:08:22 -0500

    rm not used code in read-txs.service.ts

--- workers/tx/src/task/mongo/read-txs.service.ts
+++ workers/tx/src/task/mongo/read-txs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { RawTx, TxsRange, RangeTxsStats } from '../../models';
+import { RawTx, TxsRange } from '../../models';
 import TxDoc from './transaction.interface';
 import { HelperService } from '../../helper';
 
@@ -51,25 +51,6 @@ export class ReadTxsService {
       });
   }
 
-  private doSyncCheck(check) {
-    const { height, txs } = check;
-    this.helper.logInfoMsg(`Is checking to MongoDB ${height}...`);
-    return this.model
-      .aggregate([
-        {
-          $match: { blockNumber: height },
-        },
-        { $project: { blockNumber: 1 } },
-        {
-          $count: 'total',
-        },
-      ])
-      .then(data => {
-        if (!data.length) return false;
-        return data[0].total === txs;
-      });
-  }
-
   getTxTrackStartHeight(): Promise<number> {
     this.helper.logInfoMsg(`Getting the start track height at MongoDB`);
 
@@ -89,85 +70,6 @@ export class ReadTxsService {
         }
         this.helper.logInfoMsg(`Track height fom mongo: ${startAt}`);
         return startAt;
-      });
-  }
-
-  async getTxsStatsForRange(range: TxsRange): Promise<RangeTxsStats> {
-    this.helper.logInfo({
-      method: 'getTxsStatsForRange',
-      data: range,
-    });
-    const lTxTimeInDB = await this.getLTimeForRangeTxs(range);
-    const promises: any[] = [
-      this.getAllTxsStatsForRange(range),
-      this.getTicketTxsStatsForRange(range),
-    ];
-
-    return Promise.all(promises)
-      .then(data => {
-        const [txs, ticketTxs] = data;
-        return { txs, ticket_txs: ticketTxs };
-      })
-      .then(stats => ({ stats_at: lTxTimeInDB, stats }))
-      .then(statsData => {
-        this.helper.logInfo({
-          method: 'getTxsStatsForRange',
-          data: statsData,
-        });
-        return statsData;
-      });
-  }
-
-  getTxStatsTrackStartTime(): Promise<number> {
-    this.helper.logInfoMsg(`Get stats start time from MongoDB.`);
-    return this.model
-      .aggregate([
-        { $project: { timestamp: 1, _id: 0 } },
-        { $sort: { timestamp: 1 } },
-        { $limit: 1 },
-      ])
-      .then(records => records[0].timestamp);
-  }
-
-  private getAllTxsStatsForRange(range: TxsRange): Promise<number> {
-    return this.model
-      .aggregate([
-        { $match: { timestamp: range } },
-        { $project: { _id: 0, timestamp: 1 } },
-        { $count: 'total' },
-      ])
-      .then(records => records[0].total)
-      .then(count => {
-        this.helper.logInfoMsg(`All txs:${count}`);
-        return count;
-      });
-  }
-
-  private getTicketTxsStatsForRange(range: TxsRange): Promise<number> {
-    return this.model
-      .aggregate([
-        { $match: { timestamp: range, type: 'BuyTicketFunc' } },
-        { $project: { timestamp: 1 } },
-        { $count: 'total' },
-      ])
-      .then(records => records[0].total)
-      .then(count => {
-        this.helper.logInfoMsg(`Ticket txs:${count}`);
-        return count;
-      });
-  }
-
-  private getLTimeForRangeTxs(range: TxsRange): Promise<number> {
-    return this.model
-      .find({ timestamp: range }, { timestamp: 1 })
-      .sort({ timestamp: -1 })
-      .limit(1)
-      .then(records => {
-        if (records.length) return records[0].timestamp;
-      })
-      .then((timestamp: number) => {
-        this.helper.logInfoMsg(`LTimeForRange: ${timestamp} for tx stats`);
-        return timestamp;
       });
   }
 
@@ -224,6 +126,25 @@ export class ReadTxsService {
       .catch((e: any) => {
         this.helper.logError({ method: 'getRawTxs', e: JSON.stringify(e) });
         process.exit();
+      });
+  }
+
+  private doSyncCheck(check): Promise<boolean> {
+    const { height, txs } = check;
+    this.helper.logInfoMsg(`Is checking to MongoDB ${height}...`);
+    return this.model
+      .aggregate([
+        {
+          $match: { blockNumber: height },
+        },
+        { $project: { blockNumber: 1 } },
+        {
+          $count: 'total',
+        },
+      ])
+      .then(data => {
+        if (!data.length) return false;
+        return data[0].total === txs;
       });
   }
 }
